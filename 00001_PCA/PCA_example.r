27cdb38f90abfa7213bfea7b0b7c98d541f2fca7
Alex Sarver <40774363+ajsarver87@users.noreply.github.com>
2018-07-20 13:36:13 -0500

    Update PCA_example.r

--- 00001_PCA/PCA_example.r
+++ 00001_PCA/PCA_example.r
@@ -1,29 +1,48 @@
+#import libraries used and the mtcars data set
+library(corrplot)
+library(ggfortify)
+library(plot3D)
+library(plot3Drgl)
 df <- mtcars
-head(df)
 
-cor(df)
+#Looking at a Correlation Table for our data set
+corrplot(cor(df))
 
-model.1 <- lm(mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am + gear + carb, 
-              data = df)
-
-summary(model.1)
-
-df.pca <- prcomp(~cyl + disp + hp + drat + wt + qsec + vs + am + gear + carb,
+#Create the Principle Components(note we are only looking at the numerical data)
+df.pca <- prcomp(~ mpg + cyl + disp + hp + drat + wt + qsec + gear + carb,
                  data = df,
                  center = TRUE,
                  scale. = TRUE)
-
-
+#Will print the standard deviations and the loadings(coefficents) of each PC
 print(df.pca)
 
-plot(df.pca, type = "l")
-
+#Will display the standard deviations, proportion of variance, and cumulative proportion of each 
+#Pc
 summary(df.pca)
 
-df.pca$x[,1:5]
+#Scree Plot, Looks Like I can use 3 or 4 PC's to decribe the data, instead of all 9 variable
+plot(df.pca, type = "l")
 
-model.2 <- lm (df$mpg ~ df.pca$x[,1:5])
-summary(model.2)
+#plot the data according to the first two PCs, explains abotu 85% of the data
+autoplot(df.pca, label = TRUE, scale = 0)
 
-model.3 <- lm (df$mpg ~ df.pca$x[,1])
-summary(model.3)
+#Apply the same ideas to the Credit Card Fraud data Set
+credit <- read.csv(file = "00001_PCA/creditcard.csv",
+                   header = TRUE)
+
+credit.pca <- prcomp( ~ V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20+V21+V22+V23+V24+V25+V26+V27+V28,
+                      data = credit)
+#This double checks to see that the scores are unchanged from the orginal data frame
+#and the prcomp object (They are numberically the same, just some of the characteristics are
+#different)
+all.equal(credit[,2:29],as.data.frame(credit.pca$x))
+
+print(credit.pca)
+summary(credit.pca)
+
+screeplot(credit.pca, npcs = 28, type = c("lines"))
+
+autoplot(credit.pca, scale=0, data = credit, colour = 'Class')
+
+scatter3D(credit.pca$x[,1], credit.pca$x[,2], credit.pca$x[,3], colvar = credit$Class )
+plotrgl()
